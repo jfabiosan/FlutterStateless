ecf2449c7e8497c18333c1cbd56c35877fd50bdc
Fabio <josefabiosan@gmail.com>
2023-05-18 21:22:41 -0300

    criando do zero statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Layouts'),
-        ),
-        body: const Column(children: [
-          Center(
-            heightFactor: 2,
-            child: Text('Olá Mundo!',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Color.fromARGB(255, 218, 58, 58),
-                )),
-          ),
-        ]),
-      ),
+    return const MaterialApp(
+      title: 'Sistema de Alertas',
+      home: Home(),
     );
   }
 }
