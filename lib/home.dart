9f811cbf0bbdbc1fa982536ce9d78e0dfb0b64a9
Fabio <josefabiosan@gmail.com>
2023-05-18 22:19:22 -0300

    criando statefull

--- lib/home.dart
+++ lib/home.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int valor = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bem vindo'),
+      ),
+      body: Text('Voçê pressionou o botão $valor vezes.'),
+    );
+  }
+}
